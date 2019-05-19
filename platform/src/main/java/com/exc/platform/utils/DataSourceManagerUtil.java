465d56a5f9cf4b6bc5583ba2dc38abffc0370ed4
DexianChen <dex2015@aliyun.com>
2019-05-19 09:34:16 +0800

    feat():添加动态切换数据库工具类

---
+++ platform/src/main/java/com/exc/platform/utils/DataSourceManagerUtil.java
@@ -0,0 +1,12 @@
+package com.exc.platform.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataSourceManagerUtil {
+    private final Logger logger = LoggerFactory.getLogger(DataSourceManagerUtil.class);
+
+    public void setDataSource(String environment){
+
+    }
+}
